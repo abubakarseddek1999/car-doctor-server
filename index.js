588e64c940e73674d452c1900b84859057e8853e
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2023-10-28 10:51:05 +0600

    add functionality of bokking data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,7 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_PASS);
 
 
 
@@ -22,33 +21,105 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        const serviceCollection = client.db('carDoctor').collection('services');
+        const bookingCollection = client.db('carDoctor').collection('bookings');
+
+
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const options = {
+
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { title: 1, service_id: 1, price: 1,img:1 },
+            };
+
+
+            const result = await serviceCollection.findOne(query, options);
+            res.send(result);
+        })
+
+        // Booking
+
+        app.get('/bookings', async(req,res)=>{
+            console.log(req.query.email);
+            let query ={};
+            if(req.query?.email){
+                query ={email: req.query.email}
+            }
+
+            const result = await bookingCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/bookings', async(req, res)=>{
+            const booking= req.body;
+            console.log(booking);
+            const result =await bookingCollection.insertOne(booking);
+            res.send(result);
+
+
+        })
+
+        app.patch('/bookings/:id', async(req, res) =>{
+            const id =req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updatedBooking = req.body;
+            console.log(updatedBooking);
+            const updateDoc = {
+                $set: {
+
+                    status: updatedBooking.status
+                },
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+
+        app.delete('/bookings/:id', async(req,res)=>{
+            const id =req.params.id;
+            const query ={_id: new ObjectId(id)}
+            const result =await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('doctor is running')
 })
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Car Doctor is running on port ${port}`);
 })
